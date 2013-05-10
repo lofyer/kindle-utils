c4931378b5e0dc2ad8fc52f73d24c0cc91fcf368
demo <demo@localhost.localdomain>
2013-05-10 17:18:04 +0800

    add my mailbox

--- sendbook.sh
+++ sendbook.sh
@@ -4,4 +4,4 @@ then
 	echo "usage: sendbook.sh book"
 	exit
 fi
-echo "Book from rex-server" | mutt jiangdi007@163.com -s "Book from rex-server" -a $1
+echo "Book from rex-server" | mutt lofyer_85@kindle.com -s "Book from rex-server" -a $1
