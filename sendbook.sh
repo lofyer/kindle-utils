5267b617f61299aaa3ae7d6faaad78046cccd53d
lofyer <root@localhost.localdomain>
2013-05-14 10:25:02 +0800

    Add mailbox of kindle paperwhite

--- sendbook.sh
+++ sendbook.sh
@@ -4,4 +4,4 @@ then
 	echo "usage: sendbook.sh book"
 	exit
 fi
-echo "This is a mail from rex-server" | mutt -F muttrc lofyer_85@kinle.com -s "Book from rex-server" -a $1
+echo "This is a mail from rex-server" | mutt -F muttrc lofyer_85@free.kinle.com -s "Book from rex-server" -a $1
